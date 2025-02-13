223fe14f8e63b391415343e2b005e56e09361f58
Karolina Tchiling <kdt@my.yorku.ca>
2025-02-13 12:04:15 -0500

    Added tailwind themes and changed browser tab name/icon

--- frontend/src/pages/CommunityPage.tsx
+++ frontend/src/pages/CommunityPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const CommunityPage: React.FC = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-      <p className="text-red-500 text-3xl font-bold">
+      <p className="text-red text-3xl border p-8 b-red rounded-full">
         Hello, from community page
       </p>
     </main>
