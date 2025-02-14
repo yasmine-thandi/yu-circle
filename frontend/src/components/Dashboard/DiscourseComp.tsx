6791f45e49cd05603a5900099588223de1947789
Karolina Tchiling <kdt@my.yorku.ca>
2025-02-13 19:11:47 -0500

    Completed profile section of dashboard

---
+++ frontend/src/components/Dashboard/DiscourseComp.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DiscourseComp: React.FC = () => {
+  return (
+
+    <main className="flex flex-col items-center">
+
+      <p className="text-red p-8 border b-red rounded-lg w-full">
+        Hello, from dashboard page
+      </p>
+    </main>
+  );
+};
+
+export default DiscourseComp;
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import ProfileComp from "../components/Dashboard/ProfileComp";
+import SettingComp from "../components/Dashboard/SettingComp";
+import DiscourseComp from "../components/Dashboard/DiscourseComp";
+import MarketplaceComp from "../components/Dashboard/MarketplaceComp";
 
 const DashboardPage: React.FC = () => {
   return (
     <>
     <Header />
-    <main className="flex flex-col items-center">
+    <main className="grid grid-cols-2 my-6 mx-12 gap-4">
+      <ProfileComp />
+      <SettingComp />
+      <DiscourseComp />
+      <MarketplaceComp />
 
-      <p className="text-red text-3xl p-8 m-8 border b-red rounded-full">
-        Hello, from dashboard page
-      </p>
     </main>
     </>
   );
